82b8be4d6a5bfef6b9fe9588ac1a5e62eaea3e32
Nico <nicolas.gastellu@mail.mcgill.ca>
2024-05-06 18:23:19 -0400

    `random_cell.py` now takes in command line args

--- random_bravais/random_cell.py
+++ random_bravais/random_cell.py
@@ -15,18 +15,24 @@ np.random.seed(nn)
 rank = MPI.COMM_WORLD.Get_rank()
 nprocs = MPI.COMM_WORLD.Get_size()
 
+rho = 65000 / (400*400) #approximate nb of atoms per angstrom^2 in MAC
 
-a = 20 #size of unit cell
-l = 18 #span of random coords in unot cell
-n = 40 #number of atoms per cell
+a = int(sys.argv[2]) #size of unit cell
+l = int(sys.argv[3]) #span of random coords in unot cell
+#n = int(rho * a * a) #number of atoms per cell
+n = int(rho * l * l) #number of atoms per cell
+
+print(f'******* Points per cell = {n} *******')
+
 nsamples = 50
+ncells = 100
 
 
 latt_vecs = np.eye(2) * a
 
 cell = np.random.rand(2,n) * l
 
-supercell = make_supercell(cell,latt_vecs,50,50)
+supercell = make_supercell(cell,latt_vecs,ncells,ncells)
 
 supercell = supercell.T
 
@@ -44,8 +50,8 @@ print(f'Coord bounds along y-direction: {[ly,Ly]}',flush=True)
 
 tree = cKDTree(supercell)
 
-nradii = 1000
-rmax = 350
+rmax = (ncells * a) / 3
+nradii = 5 * rmax
 print(f'Max sample window size = {rmax}', flush=True)
 print(f'Number of radii = {nradii} --> dr = {(rmax-1)/nradii}',flush=True)
 
@@ -64,5 +70,5 @@ dfs = np.zeros(radii.shape[0],dtype=float)
 for k,r in enumerate(radii):
     dfs[k] = DensityFluctuationsRS(tree,r,[lx,Lx],[ly,Ly],nsamples)
 
-np.save('dfs-%d.npy'%(rank),dfs) #distinguish the output of each process by its number
-np.save('radii-%d.npy'%rank,radii)
+np.save('dfs_a%d_l%d_n%d-%d.npy'%(a,l,n,rank),dfs) #distinguish the output of each process by its number
+np.save('radii_a%d_l%d_n%d-%d.npy'%(a,l,n,rank),radii)
